85c4f3597559ab759fcc072310213c1420dcb0a9
mazen103501 <mazen103501@gmail.com>
2024-09-18 01:05:17 +0300

    Replace BrowserRouter with HashRouter for gh-pages to work

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Search from "./components/Search";
